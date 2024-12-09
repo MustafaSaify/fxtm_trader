a2cbd5ffd78cdf868081ab24a0694bec24df7b4a
MustafaSaify <mustafa.saify04@gmail.com>
2024-12-09 10:16:50 +0400

    added placeholder tests for datasource and repository

--- lib/src/features/forex_tracker/data/datasource/forex_symbol_datasource.dart
+++ lib/src/features/forex_tracker/data/datasource/forex_symbol_datasource.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
+
 abstract class ForexSymbolDataSource {
   Future<List<dynamic>> getSymbols({required String exchange});
+}
+
+class ForexSymbolDataSourceImpl implements ForexSymbolDataSource {
+
+  final HttpClient _httpClient;
+
+  ForexSymbolDataSourceImpl({required HttpClient httpClient}) : _httpClient = httpClient;
+  
+  @override
+  Future<List<dynamic>> getSymbols({required String exchange}) async => [];
 }
---
+++ test/src/features/forex_tracker/data/repository/forex_symbols_repository_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fxtm_trader/src/features/forex_tracker/data/datasource/forex_symbol_datasource.dart';
+import 'package:fxtm_trader/src/features/forex_tracker/data/repository/forex_symbols_repository_impl.dart';
+import 'package:fxtm_trader/src/features/forex_tracker/domain/repository/forex_symbols_repository.dart';
+import 'package:mockito/annotations.dart';
+
+import 'forex_symbols_repository_test.mocks.dart';
+
+@GenerateMocks([ForexSymbolDataSource])
+void main() {
+  late ForexSymbolsRepository sut;
+  late ForexSymbolDataSource dataSourceMock;
+
+  setUp(() {
+    dataSourceMock = MockForexSymbolDataSource();
+    sut = ForexSymbolsRepositoryImpl(symbolDataSource: dataSourceMock);
+  });
+
+  group('Test forex_symbols_repository - Happy Cases', () {
+    test('Get Symbols - Success case', () async {
+
+    });
+  });
+
+  group('Test forex_symbols_repository - Error Cases', () {
+    test('Get Symbols - Failure case', () async {
+
+    });
+
+    test('Get Symbols - Empty case', () async {
+
+    });
+  });
+}
