0bbb077b6e2ddc0ce84de1fcd8a893c6115a73bf
MustafaSaify <mustafa.saify04@gmail.com>
2024-12-16 22:44:24 +0400

    removed unwanted code

--- lib/src/features/forex_tracker/data/models/forex_price_query.dart
+++
@@ -1,8 +0,0 @@
-class ForexPriceQuery {
-  final String type;
-  final String symbol;
-
-  ForexPriceQuery({required this.type, required this.symbol});
-
-  Map<String, String> toJson() => {"type": "subscribe", "symbol": "OANDA:EUR_TRY"};
-}
